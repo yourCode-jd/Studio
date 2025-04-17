e27853e5bcedac3966c60b17da1adfb0df1e292d
jaspreet tpss <jaspreetstpss@gmail.com>
2025-04-17 22:08:01 +0530

    all pages design and update the content

--- js/main.js
+++ js/main.js
@@ -14,31 +14,6 @@
   // Initiate the wowjs
   new WOW().init();
 
-  // Navbar on scrolling
-  // $(window).scroll(function () {
-  //     if ($(this).scrollTop() > 300) {
-  //         $('.navbar').fadeIn('slow').css('display', 'flex');
-  //     } else {
-  //         $('.navbar').fadeOut('slow').css('display', 'none');
-  //     }
-  // });
-
-  // Modal Video
-  var $videoSrc;
-  $(".btn-play").click(function () {
-    $videoSrc = $(this).data("src");
-  });
-  console.log($videoSrc);
-  $("#videoModal").on("shown.bs.modal", function (e) {
-    $("#video").attr(
-      "src",
-      $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
-    );
-  });
-  $("#videoModal").on("hide.bs.modal", function (e) {
-    $("#video").attr("src", $videoSrc);
-  });
-
   // Back to top button
   $(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
